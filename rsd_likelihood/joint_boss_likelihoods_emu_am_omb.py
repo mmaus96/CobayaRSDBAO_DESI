860b7fcd44d4529855c6c014cfb9e135fb80c090
mmaus <mark.maus@berkeley.edu>
2023-06-22 19:04:46 -0700

    added likelihood script

---
+++ rsd_likelihood/joint_boss_likelihoods_emu_am_omb.py
@@ -0,0 +1,10 @@
+from joint_likelihood_gc_emu_am_omb import JointLikelihood
+
+class FullShapePlusBAOEmu_Z3(JointLikelihood):
+    pass
+
+class FullShapePlusBAOEmu_Z1(JointLikelihood):
+    pass
+
+class FS_BAOEmu_Abacus_LRG_Cubic_mean(JointLikelihood):
+    pass
